57a799a5a8b5969c1d84383eb967de16be1b2ab4
manoj on pc <er_manojbisht@yahoo.com>
2025-11-27 09:46:45 +0530

    m

--- resources/views/layouts/layout001/app.blade.php
+++ resources/views/layouts/layout001/app.blade.php
@@ -183,6 +183,7 @@
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     @stack('scripts')
+    @stack('modals')
     <script>
         // Select2 initialized locally where needed
     </script>
